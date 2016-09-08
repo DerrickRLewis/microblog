b366ea16e8019e906524f5f917946c6449ded182
Derrick <derrickrlewis@gmail.com>
2016-09-08 08:57:20 -0500

    Added edit feature

--- src/main/java/com/theironyard/BlogController.java
+++ src/main/java/com/theironyard/BlogController.java
@@ -1,17 +1,12 @@
 package com.theironyard;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,34 +17,33 @@ import java.util.List;
 public class BlogController {
 
 
-@Autowired
+    @Autowired
     BlogRepository message;
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home(Model model, HttpSession session){
+    public String home(Model model, HttpSession session) {
 
         List<Message> messageList = message.findAll();
 
-//        model.addAttribute("name", session.getAttribute("userName"));
-        model.addAttribute("messages",messageList);
-//        model.addAttribute("messages",session.getAttribute("messages"));
-        model.addAttribute("updateMessage" ,session.getAttribute("updateMessage"));
+        model.addAttribute("name", session.getAttribute("userName"));
+        model.addAttribute("messages", messageList);
+        model.addAttribute("messages",session.getAttribute("messages"));
+        model.addAttribute("updateMessage", session.getAttribute("updateMessage"));
         return "home";
     }
 
 
-////    @RequestMapping(path ="/login", method = RequestMethod.POST)
-////    public String login (HttpSession session, String userName){
-////        session.setAttribute("userName",userName);
-////        return "redirect:/";
-//    }
-
+    @RequestMapping(path ="/login", method = RequestMethod.POST)
+    public String login (HttpSession session, String userName){
+        session.setAttribute("userName",userName);
+        return "redirect:/";
+    }
 
 
     @RequestMapping(path = "/add-message", method = RequestMethod.POST)
-    public String inputMessage (HttpSession session, String text ){
-        session.setAttribute("messages",text);
+    public String inputMessage(HttpSession session, String text) {
+        session.setAttribute("messages", text);
         message.save(new Message(text));
 
         return "redirect:/";
@@ -58,25 +52,16 @@ public class BlogController {
     /**
      * edit feature is not working
      * looking for fix to this issue
-      */
+     */
 
-    @RequestMapping(path ="/edit-message", method = RequestMethod.POST)
-    public  String editMessage(HttpSession session , int id,String text){
-
-
-//        session.setAttribute("deleteMessage",message.get(id));
-//        message.remove(id);
-
-        session.setAttribute("messages",text);
-        message.findOne(id);
-
-
-
-
-
-        //message.delete(-1);
+    @RequestMapping(path = "/edit-message", method = RequestMethod.POST)
+    public String edit(String text, Integer id) {
+        if (!text.isEmpty()) {
+            message.delete(id);
+            message.save(new Message(text));
+        }
         return "redirect:/";
-   }
+    }
 
 
 }
